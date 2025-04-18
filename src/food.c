6293b3b8ec2e22a350122e076f07b2436c061631
ZhangDongzai <3187480352@qq.com>
2025-04-18 23:27:00 +0800

    perf: food won't appear on snake's body

--- src/food.c
+++ src/food.c
@@ -3,7 +3,27 @@
 int food[2];
 
 void freshFood() {
-    food[0] = SDL_rand(10); food[1] = SDL_rand(10);
+    int map[MAP_WIDTH][MAP_HEIGHT] = {0};
+    for (int i = 0; i < snake.length; i++) {
+        map[snake.body[i][0]][snake.body[i][1]] = 1;
+    }
+    
+    int total = MAP_WIDTH * MAP_HEIGHT - snake.length;
+    int target = SDL_rand(total);
+
+    int count = 0;
+    for (int x = 0; x < MAP_WIDTH; x++) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            if (map[x][y] == 0) {
+                if (count == target) {
+                    food[0] = x;
+                    food[1] = y;
+                    return;
+                }
+                count++;
+            }
+        }
+    }
 }
 
 void renderFood() {
